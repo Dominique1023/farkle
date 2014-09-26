1e93de4745b3c7bc96378885ae503271d68cf168
dominique <dominiquev91@gmail.com>
2014-09-26 15:00:24 -0700

    Initial Commit

---
+++ Farkle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Farkle
+//
+//  Created by Dominique on 9/26/14.
+//  Copyright (c) 2014 dominique vasquez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Farkle/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Farkle
+//
+//  Created by Dominique on 9/26/14.
+//  Copyright (c) 2014 dominique vasquez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
