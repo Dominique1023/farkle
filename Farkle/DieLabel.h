58291275042a73b1a965f4f0802f153580d3668e
dominique <dominiquev91@gmail.com>
2014-09-26 15:04:23 -0700

    First Commit

---
+++ Farkle/DieLabel.h
@@ -0,0 +1,13 @@
+//
+//  DieLabel.h
+//  Farkle
+//
+//  Created by Dominique on 9/26/14.
+//  Copyright (c) 2014 dominique vasquez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DieLabel : UILabel
+
+@end
